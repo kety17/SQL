5cd1ae5ee64534c7069015fe1b87fa0182a6ee51
kety17 <115723210+kety17@users.noreply.github.com>
2023-02-07 08:53:03 -0300

    Add files via upload

---
+++ DAL/TBLOGINDAO.cs
@@ -0,0 +1,23 @@
+﻿using SistemaGURI.MODELS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGURI.DAL
+{
+    public class TBLODINDAO
+    {
+
+        public List<TBLOGIN> GetTodosLogins()
+        {
+            SqlConnection conexao = new SqlConnection(@"Server=.\SENAI2022; Database=BDJURIDICO; User Id =sa; Password=senai.123;");
+
+            string sql = " select * from TBLOGIN";
+            var dados = (List<TBLOGIN>)conexao.Query<TBLOGIN>(sql);
+
+            return dados;
+        }
+    }
+}
---
+++ MODELS/TBLOGIN.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGURI.MODELS
+{
+    public class TBLOGIN
+    {
+        public int LOGID { get; set; }
+        public string LOGNOME { get; set; }
+        public string LOGSENHA { get; set; }
+    }
+}
+
